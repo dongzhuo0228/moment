38d8dc48fe691a99d1a40c61d16bf2f5221b0dab
FantasticMao <maomao8017@gmail.com>
2019-01-10 22:12:18 +0800

    :expressionless:

---
+++ bin/deply.sh
@@ -0,0 +1,25 @@
+# macOS
+
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home
+appVersion='1.0'
+
+srcdir=../target
+srcfiles=moment-1.0-jar-with-dependencies.jar
+
+outdir=../target/javapackager/macOS
+outfile=moment.dmg
+
+appclass='cn.fantasticmao.util.moment.Main'
+name=moment
+vendor=FantasticMao
+description='一个简单的图片拼接小工具'
+
+javapackager -deploy \
+-native dmg \
+-Bruntime=$JAVA_HOME \
+-BappVersion=$appVersion \
+-srcdir $srcdir -srcfiles $srcfiles \
+-outdir $outdir -outfile $outfile \
+-appclass $appclass \
+-name $name -vendor $vendor -description $description \
+-v
